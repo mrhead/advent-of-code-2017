f7f48e444814a7437438e9bcad8561398ec7400a
Patrik Bóna <patrik.bona@mrhead.sk>
2017-12-15 21:43:52 +0100

    Day 2

---
+++ day2.rb
@@ -0,0 +1,24 @@
+def checksum(spreadsheet)
+  spreadsheet.lines.map do |row|
+    columns = row.split.map(&:to_i)
+
+    min = columns.min
+    max = columns.max
+
+    max - min
+  end.sum
+end
+
+require "minitest/autorun"
+
+class Tests < MiniTest::Unit::TestCase
+  def test_example_input
+    input = <<~SPREADSHEET
+      5 1 9 5
+      7 5 3
+      2 4 6 8
+    SPREADSHEET
+
+    assert_equal 18, checksum(input)
+  end
+end
